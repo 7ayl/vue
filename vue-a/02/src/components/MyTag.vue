0c349445096826a54f9556a26f4db5687288f6d8
7ayl <7ayl@qq.com>
2024-10-18 22:13:46 +0800

    committwo

---
+++ vue-a/02/src/components/MyTag.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="my-tag">
+        <input
+          ref="inp"
+          v-if="isEdit" 
+          v-focus
+          class="input"
+          type="text"
+          placeholder="输入标签"
+          @blur="isEdit=false"
+        />
+        <div 
+          v-else 
+          @dblclick="handleClick"
+          class="text">
+            茶具
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            isEdit:false
+        }
+        
+    },
+    methods:{
+        handleClick(){
+            //双击后，切换到显示状态(Vue是异步dom更新)
+            this.isEdit=true
+
+            //等dom更新完了，再获取焦点
+            // this.$nextTick(()=>{
+            //     //立刻获取焦点
+            //     this.$refs.inp.focus()
+            // })
+            
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .my-tag {
+    cursor: pointer;
+    .input {
+      appearance: none;
+      outline: none;
+      border: 1px solid #ccc;
+      width: 100px;
+      height: 40px;
+      box-sizing: border-box;
+      padding: 10px;
+      color: #666;
+      &::placeholder {
+        color: #666;
+      }
+    }
+  }
+</style>
---
+++ vue-fifth/src/02/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- 主体区域 -->
+  <section id="app">
+    <TodoHeader @add="handleAdd"></TodoHeader>
+    <TodoMain @del="handleDel" :list="list"></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+//渲染功能：
+// 1.提供数据？ -> 提供在公共的父组件 App.vue
+// 2.通过父传子，将数据传递给TodoMain
+// 3.利用v-for渲染
+
+//添加功能：
+// 1.收集表单数据 ->v-model
+// 2.监听事件(回车+点击 都要进行添加)
+// 3.子传父，将任务名称传递给父组件App.vue
+// 4.进行添加 unshift(自己的数据自己负责)
+
+// 删除功能：
+// 1.监听事件(监听删除的点击) 携带 id
+// 2.子传父，将删除id传递给父组件App.vue
+// 3.进行删除 filter (自己的数据自己负责)
+
+export default {
+  data () {
+    return {
+      list:[
+        {id:1,name:'约会'},
+        {id:2,name:'亲亲'},
+        {id:3,name:'贴贴'},
+      ]
+    }
+  },
+  methods:{
+    handleAdd(todoName){
+      this.list.unshift({
+        id: +new Date(),
+        name:todoName
+      })
+    },
+    handleDel(id){
+      this.list=this.list.filter(item => item.id !== id)
+    }
+  },
+  components:{
+    TodoHeader,
+    TodoMain,
+    TodoFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
